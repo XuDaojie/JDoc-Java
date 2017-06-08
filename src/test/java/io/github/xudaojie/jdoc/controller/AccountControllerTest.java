29dee65e12246d9847c6d6bfc8375b9ade38b978
XuDaojie <xudaojieg@gmail.com>
2017-06-08 18:01:06 +0800

    First JUnit

--- src/test/java/io/github/xudaojie/jdoc/controller/AccountControllerTest.java
+++ src/test/java/io/github/xudaojie/jdoc/controller/AccountControllerTest.java
@@ -1,33 +1,28 @@
 package io.github.xudaojie.jdoc.controller;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Created by xdj on 2017/6/4.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-//@RunWith(SpringRunner.class)
-@WebMvcTest(AccountController.class)
+//@RunWith(SpringJUnit4ClassRunner.class) SpringRunner extends SpringJUnit4ClassRunner
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AccountControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private AccountController mController;
 
     @Test
     public void login() throws Exception {
-        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello World")));
+        Assert.assertNotNull(mController); // JUnit
+        Assertions.assertThat(mController).isNotNull(); // AssertJ
     }
 
 }
