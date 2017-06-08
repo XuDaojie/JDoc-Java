4a6c028307a073194d9a6fd27e099601154ec35c
XuDaojie <xudaojieg@gmail.com>
2017-06-08 14:52:13 +0800

    test.

---
+++ src/test/java/io/github/xudaojie/jdoc/controller/AccountControllerTest.java
@@ -0,0 +1,33 @@
+package io.github.xudaojie.jdoc.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by xdj on 2017/6/4.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+//@RunWith(SpringRunner.class)
+@WebMvcTest(AccountController.class)
+public class AccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void login() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello World")));
+    }
+
+}
